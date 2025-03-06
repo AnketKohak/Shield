99e7495c0a9cafeacbd7b594394465cf33c79e1c
AnketKohak <anketkohak40@gmail.com>
2025-03-06 11:53:40 +0530

    Initial Commit

---
+++ Shield/Shield.h
@@ -0,0 +1,18 @@
+//
+//  Shield.h
+//  Shield
+//
+//  Created by Protectt_intern on 06/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shield.
+FOUNDATION_EXPORT double ShieldVersionNumber;
+
+//! Project version string for Shield.
+FOUNDATION_EXPORT const unsigned char ShieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shield/PublicHeader.h>
+
+
